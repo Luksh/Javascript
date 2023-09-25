d3832f8a5baf8469aea8316c615ad4039c1d558e
Laxman Maharjan <hotmale_luksh@hotmail.com>
2023-09-25 16:10:29 +0545

    variable hosting

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+var person = "laxman";
+person = "Laxman Maharjan";
+console.log(person);
+/*
+This is comment
+*/
+let name = "Shristi";
+name = "Samjhana";
+console.log(name);
+const pi = 3.14159;
+const sample = "Anything";
+console.log(sample);
